8c6fa17083806c2951b1da5048ec877d2f432877
andriant0w <andriantowicaksono90@gmail.com>
2023-03-08 15:59:52 +0700

    upload praktikum

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,18 @@
+<?php 
+$domisili = ['Jakarta', 'Bogor', 'Depok', 'Tangerang', 'Bekasi'];
+
+$program_studi = [
+    "SI" => "sistem informasi",
+    "TI" => "teknik informasi",
+    "BD" => "bisnis digital"
+    ];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "PHP"=>30,
+    "Python"=>30
+    ];
+
+?>
